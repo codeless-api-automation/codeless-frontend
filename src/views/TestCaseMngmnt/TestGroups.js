58460e3cbbd67c839ae30fef93946a21c283953f
Valentyn_Kvasov <valentynkvasov@gmail.com>
2020-03-16 22:06:46 +0100

    clear redundant data

--- src/views/TestCaseMngmnt/TestGroups.js
+++ src/views/TestCaseMngmnt/TestGroups.js
@@ -1,17 +1,14 @@
 import React from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import BugReport from "@material-ui/icons/BugReport";
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import Checkbox from '@material-ui/core/Checkbox';
 // core components
-import GridItem from "components/Grid/GridItem.js";
-import GridContainer from "components/Grid/GridContainer.js";
+
 import Button from "components/CustomButtons/Button.js";
 import ComboBox from "components/Combobox/Combobox.js";
 import Input from "components/CustomInput/CustomInput.js";
@@ -20,19 +17,6 @@ import CustomInput from "components/CustomInput/CustomInput.js";
 import Grid from '@material-ui/core/Grid';
 
 import { InputLabel, FormControl, TextField, MenuItem, Select } from '@material-ui/core';
-
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
 
 const useStyles = makeStyles(theme => ({
   root: {
