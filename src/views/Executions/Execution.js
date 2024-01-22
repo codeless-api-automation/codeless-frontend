173ff99a7a1d9b0e2fbe927016a5ff56715a0007
Oleg Strunevskiy <oleg.strunevskiy@gmail.com>
2024-01-21 23:04:42 -0800

    Remove unused code

--- src/views/Executions/Execution.js
+++ src/views/Executions/Execution.js
@@ -190,21 +190,6 @@ function CustomizedAccordions(props) {
     return row.includes(RESPONSE_BODY_LOG_ENTRY) || row.includes(REQUEST_HEADERS_LOG_ENTRY) || row.includes(RESPONSE_HEADERS_LOG_ENTRY);
   }
 
-  const getContentType = (row) => {
-    let headers = row.split(RESPONSE_HEADERS_LOG_ENTRY)[1].split("\r\n")
-
-    let contentType = headers
-      .map((header) =>
-        [
-          header.substring(0, header.indexOf("=")).toLowerCase(),
-          header.substring(header.indexOf("=") + 1).toLowerCase()
-        ]
-      )
-      .filter(entry => entry[0] === "content-type")
-
-    return contentType[1]
-  }
-
   const isResponseBody = (row) => {
     return row.includes(RESPONSE_BODY_LOG_ENTRY)
   }
@@ -225,7 +210,6 @@ function CustomizedAccordions(props) {
             <DecoratedAccordionDetails>
               {isResponseBody(row)
                 ? <ResponseBody
-                  contentType={getContentType(rows[index - 1])}
                   response={getDetails(row)}
                 />
                 : <Details
